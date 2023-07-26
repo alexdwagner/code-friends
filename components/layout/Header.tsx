6d1b159ff151a70d0a8eab754f440664557c6f16
Alex Wagner <alex.wagner5@gmail.com>
2023-07-26 17:08:38 -0400

    Some changes to nav.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,28 +1,10 @@
-// components/Header.tsx
-
 import React from 'react';
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faPlus } from '@fortawesome/free-solid-svg-icons';
 import Nav from './Nav';
 
 const Header: React.FC = () => {
     return (
         <header className="header bg-gray-100 p-4 flex justify-between items-center">
-            <div className="flex items-center space-x-4">
-                <Nav />
-            </div>
-            
-            <div className="relative group flex items-center">
-                <Link href="/create" legacyBehavior>
-                    <a className="group-hover:underline flex items-center">
-                        <span className="absolute right-full mr-2 p-2 bg-white text-gray-800 text-xs rounded shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-                            Create a post
-                        </span>
-                        <FontAwesomeIcon icon={faPlus} width="24" height="24" className="mr-4" />
-                    </a>
-                </Link>
-            </div>
+            <Nav />
         </header>
     );
 };
--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,37 +1,47 @@
-// components/Nav.tsx
-
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBell } from '@fortawesome/free-solid-svg-icons';
+import { useRouter } from 'next/router';
 
 const Nav: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
-
+    const router = useRouter();
+    
     return (
-        <nav className="relative">
+        <nav className="relative flex items-center space-x-4 justify-between w-full">
             <FontAwesomeIcon icon={faBars} width="24" height="24" onClick={() => setIsOpen(!isOpen)} />
+
+            {router.pathname !== '/create' && (
+                <Link href="/create" legacyBehavior>
+                    <a className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors duration-200">
+                        Make a Post
+                    </a>
+                </Link>
+            )}
+
+            <FontAwesomeIcon icon={faBell} width="24" height="24" />
+
             {isOpen && (
                 <ul className="absolute left-0 mt-2 space-y-2 bg-white text-blue-600 border border-gray-200 rounded shadow-lg">
                     <li className="px-4 py-2 hover:bg-gray-200">
-                        <Link href="/" legacyBehavior>
-                            <a onClick={() => setIsOpen(false)}>Home</a>
+                        <Link href="/profile" legacyBehavior>
+                            <a onClick={() => setIsOpen(false)}>Profile</a>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-200">
-                        <Link href="/about" legacyBehavior>
-                            <a onClick={() => setIsOpen(false)}>About</a>
+                        <Link href="/settings" legacyBehavior>
+                            <a onClick={() => setIsOpen(false)}>Settings</a>
                         </Link>
                     </li>
                     <li className="px-4 py-2 hover:bg-gray-200">
-                        <Link href="/posts" legacyBehavior>
-                            <a onClick={() => setIsOpen(false)}>Posts</a>
-                        </Link>
-                    </li>
-                    <li className="px-4 py-2 hover:bg-gray-200">
-                        <Link href="/categories" legacyBehavior>
-                            <a onClick={() => setIsOpen(false)}>Categories</a>
-                        </Link>
+                        {/* Placeholder for logout logic */}
+                        <a onClick={() => {
+                            setIsOpen(false);
+                            // Add logout logic here
+                        }}>
+                            Logout
+                        </a>
                     </li>
                 </ul>
             )}
